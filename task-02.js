49b259a18bb03070e194b7cad2aa0ffa01d4146d
Việt Hưng <hn993533@gmail.com>
2025-08-03 16:07:36 +0700

    Xoa toan bo

--- task-02.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by trungquandev.com's author on Nov 07 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
-// Done =))
